8140df2174e4002b01fd144ec9c62b2fc73f3761
Esther Lainez <esthertarazona81@gmail.com>
2019-02-23 11:57:33 +0100

    text

---
+++ src/suma.java
@@ -0,0 +1,16 @@
+package entornos;
+
+public class suma {
+
+	public suma() {
+		
+	}
+	
+	
+	public static int SumaPositiva(int a,int b) {
+		
+		int suma=a+b;
+		return suma;
+	}
+
+}
---
+++ src/sumaa_text.java
@@ -0,0 +1,10 @@
+package entornos;
+
+public class sumaa_text {
+	
+	//@Test 
+	public void SumaPositiva() {	
+		suma s=new suma();
+		//assertTest(s.SumaPositiva(2,3)==5);
+	}
+}
